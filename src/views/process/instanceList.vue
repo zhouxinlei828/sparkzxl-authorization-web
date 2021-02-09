3b68227c2ab24ff4df40c12a90fcc9b72ac32be5
周鑫磊 <charles.zhou@farben.com.cn>
2021-02-09 16:53:24 +0800

    代码优化

--- src/views/process/instanceList.vue
+++ src/views/process/instanceList.vue
@@ -1,127 +1,120 @@
 <template>
-  <el-card>
-    <div class="filter-container" style="padding-bottom: 0">
-      <el-form :inline="true" :model="queryParam" size="small">
-        <el-form-item label="流程名称:">
-          <el-input
-            v-model="queryParam.name"
-            placeholder="请输入流程流程名称"
-          />
-        </el-form-item>
-        <el-form-item>
-          <el-button
-            style="margin-left: 12px"
-            icon="search"
-            type="primary"
-            @click="getProcessInstanceList"
+  <div class="filter-container" style="padding-bottom: 0">
+    <el-form :inline="true" :model="queryParam" size="small">
+      <el-form-item label="流程名称:">
+        <el-input v-model="queryParam.name" placeholder="请输入流程流程名称" />
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          style="margin-left: 12px"
+          icon="search"
+          type="primary"
+          @click="getProcessInstanceList"
+        >
+          查询
+        </el-button>
+        <el-button
+          style="margin-left: 12px"
+          @click="
+            () =>
+              (this.queryParam = {
+                pageNum: 1,
+                pageSize: 10,
+                name: '',
+              })
+          "
+        >
+          重置
+        </el-button>
+      </el-form-item>
+    </el-form>
+    <el-divider content-position="left">结果列表</el-divider>
+    <el-table
+      v-loading="tableLoading"
+      element-loading-text="拼命加载中"
+      element-loading-spinner="el-icon-loading"
+      :data="modelData"
+      border
+      style="width: 100%"
+      max-height="450"
+    >
+      <el-table-column
+        prop="processInstanceId"
+        label="流程实例id"
+      ></el-table-column>
+      <el-table-column prop="businessKey" label="业务主键"></el-table-column>
+      <el-table-column prop="processName" label="流程名称"></el-table-column>
+      <el-table-column prop="processKey" label="流程key"></el-table-column>
+      <el-table-column prop="status" label="流程状态"></el-table-column>
+      <el-table-column prop="suspensionState" label="是否挂起">
+        <template #default="{ row }">
+          <el-tag
+            :type="row.suspensionState.id === 2 ? 'danger' : 'primary'"
+            disable-transitions
           >
-            查询
-          </el-button>
-          <el-button
-            style="margin-left: 12px"
-            @click="
-              () =>
-                (this.queryParam = {
-                  pageNum: 1,
-                  pageSize: 10,
-                  name: '',
-                })
-            "
-          >
-            重置
-          </el-button>
-        </el-form-item>
-      </el-form>
-      <el-divider content-position="left">结果列表</el-divider>
-      <el-table
-        v-loading="tableLoading"
-        element-loading-text="拼命加载中"
-        element-loading-spinner="el-icon-loading"
-        :data="modelData"
-        border
-        style="width: 100%"
-        max-height="450"
-      >
-        <el-table-column
-          prop="processInstanceId"
-          label="流程实例id"
-        ></el-table-column>
-        <el-table-column prop="businessKey" label="业务主键"></el-table-column>
-        <el-table-column prop="processName" label="流程名称"></el-table-column>
-        <el-table-column prop="processKey" label="流程key"></el-table-column>
-        <el-table-column prop="status" label="流程状态"></el-table-column>
-        <el-table-column prop="suspensionState" label="是否挂起">
-          <template #default="{ row }">
-            <el-tag
-              :type="row.suspensionState.id === 2 ? 'danger' : 'primary'"
-              disable-transitions
+            {{ row.suspensionState.name }}
+          </el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="startTime" label="创建时间"></el-table-column>
+      <el-table-column prop="originator" label="发起人"></el-table-column>
+      <el-table-column prop="finishTime" label="完成时间"></el-table-column>
+      <el-table-column prop="dueTime" label="耗时"></el-table-column>
+      <el-table-column label="操作" align="center" width="200">
+        <template #default="{ row }">
+          <el-link type="primary">
+            <el-button
+              type="text"
+              @click="getProcessHistory(row.processInstanceId)"
             >
-              {{ row.suspensionState.name }}
-            </el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column prop="startTime" label="创建时间"></el-table-column>
-        <el-table-column prop="originator" label="发起人"></el-table-column>
-        <el-table-column prop="finishTime" label="完成时间"></el-table-column>
-        <el-table-column prop="dueTime" label="耗时"></el-table-column>
-        <el-table-column label="操作" align="center" width="200">
-          <template #default="{ row }">
-            <el-link type="primary">
-              <el-button
-                type="text"
-                @click="getProcessHistory(row.processInstanceId)"
-              >
-                查看
-              </el-button>
-            </el-link>
-            <el-divider direction="vertical"></el-divider>
-            <el-link type="primary">
-              <el-button
-                type="text"
-                @click="handlerDeleteProcess(row.processInstanceId)"
-              >
-                删除
-              </el-button>
-            </el-link>
-            <el-divider direction="vertical"></el-divider>
-            <span
-              v-show="row.status !== '结束' && row.suspensionState.id !== 2"
+              查看
+            </el-button>
+          </el-link>
+          <el-divider direction="vertical"></el-divider>
+          <el-link type="primary">
+            <el-button
+              type="text"
+              @click="handlerDeleteProcess(row.processInstanceId)"
             >
-              <el-link type="primary">
-                <el-button
-                  type="text"
-                  @click="handlerSuspendProcess(row.processInstanceId)"
-                >
-                  挂起
-                </el-button>
-              </el-link>
-              <el-divider direction="vertical" />
-            </span>
+              删除
+            </el-button>
+          </el-link>
+          <el-divider direction="vertical"></el-divider>
+          <span v-show="row.status !== '结束' && row.suspensionState.id !== 2">
             <el-link type="primary">
               <el-button
                 type="text"
-                @click="handleShowFlowChart(row.processInstanceId)"
+                @click="handlerSuspendProcess(row.processInstanceId)"
               >
-                流程图
+                挂起
               </el-button>
             </el-link>
-          </template>
-        </el-table-column>
-      </el-table>
-      <el-pagination
-        background
-        :current-page="queryParam.pageNum"
-        :page-size="queryParam.pageSize"
-        :layout="layout"
-        :total="total"
-        :page-sizes="[5, 10, 20, 30]"
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-      ></el-pagination>
-      <view-flow-chart-form ref="flowChartForm" />
-      <process-history-form ref="processHistoryForm" />
-    </div>
-  </el-card>
+            <el-divider direction="vertical" />
+          </span>
+          <el-link type="primary">
+            <el-button
+              type="text"
+              @click="handleShowFlowChart(row.processInstanceId)"
+            >
+              流程图
+            </el-button>
+          </el-link>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      background
+      :current-page="queryParam.pageNum"
+      :page-size="queryParam.pageSize"
+      :layout="layout"
+      :total="total"
+      :page-sizes="[5, 10, 20, 30]"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    ></el-pagination>
+    <view-flow-chart-form ref="flowChartForm" />
+    <process-history-form ref="processHistoryForm" />
+  </div>
 </template>
 
 <script>
--- src/views/process/modelList.vue
+++ src/views/process/modelList.vue
@@ -1,121 +1,110 @@
 <template>
-  <el-card :bordered="true" :hoverable="true">
-    <div class="filter-container" style="padding-bottom: 0">
-      <el-form :inline="true" :model="queryParam" size="small">
-        <el-form-item label="流程名称:">
-          <el-input
-            v-model="queryParam.name"
-            placeholder="请输入流程流程名称"
-          />
-        </el-form-item>
-        <el-form-item label="流程KEY:">
-          <el-input v-model="queryParam.key" placeholder="请输入流程KEY" />
-        </el-form-item>
-        <el-form-item>
-          <el-button
-            style="margin-left: 12px"
-            icon="search"
-            type="primary"
-            @click="getModelList"
-          >
-            查询
-          </el-button>
-          <el-button
-            style="margin-left: 12px"
-            @click="
-              () =>
-                (this.queryParam = {
-                  pageNum: 1,
-                  pageSize: 10,
-                  name: '',
-                  key: '',
-                })
-            "
-          >
-            重置
-          </el-button>
-        </el-form-item>
-      </el-form>
-      <el-divider content-position="left">结果列表</el-divider>
-      <div class="table-operator">
-        <el-button size="small" type="primary" @click="handleAdd">
-          新建
+  <div class="filter-container" style="padding-bottom: 0">
+    <el-form :inline="true" :model="queryParam" size="small">
+      <el-form-item label="流程名称:">
+        <el-input v-model="queryParam.name" placeholder="请输入流程流程名称" />
+      </el-form-item>
+      <el-form-item label="流程KEY:">
+        <el-input v-model="queryParam.key" placeholder="请输入流程KEY" />
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          style="margin-left: 12px"
+          icon="search"
+          type="primary"
+          @click="getModelList"
+        >
+          查询
         </el-button>
-      </div>
-      <el-table
-        v-loading="tableLoading"
-        element-loading-text="拼命加载中"
-        element-loading-spinner="el-icon-loading"
-        :data="modelData"
-        border
-        style="width: 100%"
-        max-height="450"
-      >
-        <el-table-column prop="name" label="流程名称"></el-table-column>
-        <el-table-column
-          prop="key"
-          label="流程key"
-          width="180"
-        ></el-table-column>
-        <el-table-column
-          prop="description"
-          label="描述"
-          width="210"
-        ></el-table-column>
-        <el-table-column
-          prop="deployed"
-          label="状态"
-          width="80"
-        ></el-table-column>
-        <el-table-column prop="version" label="版本"></el-table-column>
-        <el-table-column label="操作" align="center" width="250">
-          <template #default="{ row }">
-            <el-link type="primary">
-              <IconFont type="icon-edit" @click="handleEdit(row.id)" />
-            </el-link>
-            <el-divider direction="vertical"></el-divider>
-            <el-link type="primary">
-              <IconFont
-                type="icon-template_delete"
-                @click="handleDelete(row.id)"
-              />
-            </el-link>
-            <el-divider direction="vertical" />
-            <el-link type="primary">
-              <el-button type="text" @click="handleSetRule(row.id)">
-                流程规则
-              </el-button>
-            </el-link>
-            <el-divider direction="vertical" />
-            <el-link type="primary">
-              <el-button type="text" @click="handlePublishProcess(row.id)">
-                发布流程
-              </el-button>
-            </el-link>
-            <el-divider direction="vertical" />
-            <el-link type="primary">
-              <el-button type="text" @click="revokePublish(row.id)">
-                撤销
-              </el-button>
-            </el-link>
-          </template>
-        </el-table-column>
-      </el-table>
-      <el-pagination
-        background
-        :current-page="queryParam.pageNum"
-        :page-size="queryParam.pageSize"
-        :layout="layout"
-        :total="total"
-        :page-sizes="[5, 10, 20, 30]"
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-      ></el-pagination>
-      <edit-flow-chart-form ref="flowChartForm" @fetch-data="getModelList" />
-      <create-model-form ref="createModelForm" @fetch-data="getModelList" />
-      <create-rule-form ref="createRuleForm" @fetch-data="getModelList" />
+        <el-button
+          style="margin-left: 12px"
+          @click="
+            () =>
+              (this.queryParam = {
+                pageNum: 1,
+                pageSize: 10,
+                name: '',
+                key: '',
+              })
+          "
+        >
+          重置
+        </el-button>
+      </el-form-item>
+    </el-form>
+    <el-divider content-position="left">结果列表</el-divider>
+    <div class="table-operator">
+      <el-button size="small" type="primary" @click="handleAdd">新建</el-button>
     </div>
-  </el-card>
+    <el-table
+      v-loading="tableLoading"
+      element-loading-text="拼命加载中"
+      element-loading-spinner="el-icon-loading"
+      :data="modelData"
+      border
+      style="width: 100%"
+      max-height="450"
+    >
+      <el-table-column prop="name" label="流程名称"></el-table-column>
+      <el-table-column prop="key" label="流程key" width="180"></el-table-column>
+      <el-table-column
+        prop="description"
+        label="描述"
+        width="210"
+      ></el-table-column>
+      <el-table-column
+        prop="deployed"
+        label="状态"
+        width="80"
+      ></el-table-column>
+      <el-table-column prop="version" label="版本"></el-table-column>
+      <el-table-column label="操作" align="center" width="250">
+        <template #default="{ row }">
+          <el-link type="primary">
+            <IconFont type="icon-edit" @click="handleEdit(row.id)" />
+          </el-link>
+          <el-divider direction="vertical"></el-divider>
+          <el-link type="primary">
+            <IconFont
+              type="icon-template_delete"
+              @click="handleDelete(row.id)"
+            />
+          </el-link>
+          <el-divider direction="vertical" />
+          <el-link type="primary">
+            <el-button type="text" @click="handleSetRule(row.id)">
+              流程规则
+            </el-button>
+          </el-link>
+          <el-divider direction="vertical" />
+          <el-link type="primary">
+            <el-button type="text" @click="handlePublishProcess(row.id)">
+              发布流程
+            </el-button>
+          </el-link>
+          <el-divider direction="vertical" />
+          <el-link type="primary">
+            <el-button type="text" @click="revokePublish(row.id)">
+              撤销
+            </el-button>
+          </el-link>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      background
+      :current-page="queryParam.pageNum"
+      :page-size="queryParam.pageSize"
+      :layout="layout"
+      :total="total"
+      :page-sizes="[5, 10, 20, 30]"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    ></el-pagination>
+    <edit-flow-chart-form ref="flowChartForm" @fetch-data="getModelList" />
+    <create-model-form ref="createModelForm" @fetch-data="getModelList" />
+    <create-rule-form ref="createRuleForm" @fetch-data="getModelList" />
+  </div>
 </template>
 
 <script>
