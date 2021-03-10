509cef27ebc3c65438d2bfe1464f702eec2ad2f8
周鑫磊 <charles.zhou@farben.com.cn>
2021-03-10 09:31:16 +0800

    代码优化

---
+++ src/views/job/jobLog.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <iframe
+      id="iframe"
+      frameborder="no"
+      allowfullscreen
+      width="100%"
+      style="overflow-y: auto"
+      height="500"
+      src="http://172.34.67.112:8080/xxl-job-admin/joblog"
+    ></iframe>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {}
+    },
+  }
+</script>
